253b8af1838e8cd0e7772eb0920495f6959a0844
aylinsglam <73175714+aylinsglam@users.noreply.github.com>
2020-11-14 13:02:52 +0300

    Add files via upload

---
+++ exerr_16.py
@@ -0,0 +1,14 @@
+#kullanıcı tarafından girilen bir sayının asal olup olmadığını kontrol edelim
+x=int(input("bir sayı giriniz: "))
+bolenler=list()
+a=int((x/2)+1)
+for i in range(2,a):
+    if x%i==0:
+        bolenler.append(i)
+if len(bolenler)==0:
+    print(x, "bir asal sayıdır.")
+else:
+    print(x, "Bir asal sayı değildir.")
+
+
+      
